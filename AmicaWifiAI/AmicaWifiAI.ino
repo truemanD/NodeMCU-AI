fb3779fe647639231d65c775304e2bcbb42d82d8
truemanD <diyanov.alex@gmail.com>
2017-08-30 17:18:33 +0300

    point+ap

--- AmicaWifiAI/AmicaWifiAI.ino
+++ AmicaWifiAI/AmicaWifiAI.ino
@@ -6,149 +6,136 @@
 
 
 int status = WL_IDLE_STATUS;
-const char *ssid = "ESPap";
-const char *password = "thereisnospoon";
-
+int statusAP = WL_IDLE_STATUS;
+const char ownSsid[] = "SmartHome";
+const char ownPassword[] = "SmartHomeModule";
 ESP8266WebServer server(80);
+const int pin = 13;
 
-void setup()
-{
+void setup() {
+  pinMode(pin, OUTPUT);
   Serial.begin(9600);
   Serial.println();
 
+  //  Serial.setDebugOutput(true);
+
+  //  create AP
+  createAccessPoint();
   //scan networks
-  int n = WiFi.scanNetworks();
-
-  String ssid;
-  uint8_t encryptionType;
-  int32_t RSSI;
-  uint8_t* BSSID;
-  int32_t channel;
-  bool isHidden;
-
-  for (int i = 0; i < n; i++)
-  {
-    WiFi.getNetworkInfo(i, ssid, encryptionType, RSSI, BSSID, channel, isHidden);
-    Serial.printf("%d: %s, Ch:%d (%ddBm) %s %s\n", i + 1, ssid.c_str(), channel, RSSI, encryptionType == ENC_TYPE_NONE ? "open" : "", isHidden ? "hidden" : "");
-  }
-
-  //try to connect to open network
-  //Serial.setDebugOutput(true);
-
-  for (int i = 0; i < n; i++)
-  {
-    WiFi.getNetworkInfo(i, ssid, encryptionType, RSSI, BSSID, channel, isHidden);
-    if (status != WL_CONNECTED)
-    {
-      if (encryptionType == ENC_TYPE_NONE)
-      {
-        Serial.print("Attempting to connect to open SSID: ");
-        Serial.println(ssid);
-        char ssid_char[ssid.length() + 1];
-        ssid.toCharArray(ssid_char, ssid.length());
-        status = WiFi.begin(ssid_char);
-        delay(1000);
-      }
-    }
-    if (status == WL_CONNECTED) {
-      Serial.print("You're connected to the network");
-    }
-  }
-
-  delay(1000);
-
-  if (status != WL_CONNECTED) {
-    Serial.print("Configuring access point...");
-    /* You can remove the password parameter if you want the AP to be open. */
-     char ssid_char[ssid.length() + 1];
-    WiFi.softAP(ssid_char);
-
-    IPAddress myIP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(myIP);
-    server.on("/", handleRoot);
-    server.begin();
-    Serial.println("HTTP server started");
-  }
-
-  //  printCurrentNet();
-  //  printWifiData();
+  scanAndConnectToNetwork();
 }
 
 void loop()
 {
-  server.handleClient();
+  status = WiFi.status();
+  if (status != WL_CONNECTED)  {
+    if (statusAP == 0) {
+      createAccessPoint();
+    }
+    scanAndConnectToNetwork();
+  }
+  if (status == WL_CONNECTED)  {
+    if (statusAP == 1) {
+      disableAccessPoint();
+    }
+  }
 
-  //  printCurrentNet();
+  blink_status();
+  server.handleClient();
 }
 
 void handleRoot() {
   server.send(200, "text/html", "<h1>You are connected</h1>");
 }
 
-//void printWifiData() {
-//  // print your WiFi shield's IP address:
-//  IPAddress ip = WiFi.localIP();
-//  Serial.print("IP Address: ");
-//  Serial.println(ip);
-//  Serial.println(ip);
-//
-//  // print your MAC address:
-//  byte mac[6];
-//  WiFi.macAddress(mac);
-//  Serial.print("MAC address: ");
-//  Serial.print(mac[5], HEX);
-//  Serial.print(":");
-//  Serial.print(mac[4], HEX);
-//  Serial.print(":");
-//  Serial.print(mac[3], HEX);
-//  Serial.print(":");
-//  Serial.print(mac[2], HEX);
-//  Serial.print(":");
-//  Serial.print(mac[1], HEX);
-//  Serial.print(":");
-//  Serial.println(mac[0], HEX);
-//
-//  // print your subnet mask:
-//  IPAddress subnet = WiFi.subnetMask();
-//  Serial.print("NetMask: ");
-//  Serial.println(subnet);
-//
-//  // print your gateway address:
-//  IPAddress gateway = WiFi.gatewayIP();
-//  Serial.print("Gateway: ");
-//  Serial.println(gateway);
-//}
-//
-//void printCurrentNet() {
-//  // print the SSID of the network you're attached to:
-//  Serial.print("SSID: ");
-//  Serial.println(WiFi.SSID());
-//
-//  // print the MAC address of the router you're attached to:
-//  byte bssid[6];
-//  WiFi.BSSID(bssid);
-//  Serial.print("BSSID: ");
-//  Serial.print(bssid[5], HEX);
-//  Serial.print(":");
-//  Serial.print(bssid[4], HEX);
-//  Serial.print(":");
-//  Serial.print(bssid[3], HEX);
-//  Serial.print(":");
-//  Serial.print(bssid[2], HEX);
-//  Serial.print(":");
-//  Serial.print(bssid[1], HEX);
-//  Serial.print(":");
-//  Serial.println(bssid[0], HEX);
-//
-//  // print the received signal strength:
-//  long rssi = WiFi.RSSI();
-//  Serial.print("signal strength (RSSI):");
-//  Serial.println(rssi);
-//
-//  // print the encryption type:
-//  byte encryption = WiFi.encryptionType();
-//  Serial.print("Encryption Type:");
-//  Serial.println(encryption, HEX);
-//}
+void createAccessPoint() {
+  Serial.print("Configuring access point for wifi network ...");
+  Serial.println(ownSsid);
+  WiFi.softAP(ownSsid, ownPassword);
+  statusAP = 1;
+  Serial.print("ESP AccessPoint IP address: 192.168.4.1; Status: ");
+  Serial.println(statusAP);
+  startServer();
+}
+
+void disableAccessPoint() {
+  Serial.print("Disabling access point ...");
+  Serial.println(ownSsid);
+  WiFi.softAPdisconnect(true);
+  statusAP = 0;
+  Serial.print("ESP AccessPoint disabled");
+}
+
+void startServer() {
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void scanAndConnectToNetwork() {
+  uint8_t encryptionType;
+  int32_t RSSI;
+  uint8_t* BSSID;
+  int32_t channel;
+  bool isHidden;
+  int n = WiFi.scanNetworks();
+  String ssid;
+
+  for (int i = 0; i < n; i++)  {
+    WiFi.getNetworkInfo(i, ssid, encryptionType, RSSI, BSSID, channel, isHidden);
+    Serial.printf("%d: %s, Ch:%d (%ddBm) %s %s\n", i + 1, ssid.c_str(), channel, RSSI, encryptionType == ENC_TYPE_NONE ? "open" : "", isHidden ? "hidden" : "");
+
+    //connect to open Wifi;
+    if (status != WL_CONNECTED)    {
+      if (encryptionType == ENC_TYPE_NONE)      {
+        connectToWifi(ssid, "");
+      }
+    }
+  }
+  WiFi.scanDelete();
+}
+
+void connectToWifi(String ssid,  String password) {
+  char ssid_char[ssid.length() + 1];
+  ssid.toCharArray(ssid_char, ssid.length() + 1);
+  if (password.length() != 0) {
+    char password_char[password.length() + 1];
+    password.toCharArray(password_char, password.length() + 1);
+    Serial.println(password_char);
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.print(ssid_char);
+    Serial.print(" with password: ");
+    Serial.println(password_char);
+    status = WiFi.begin(ssid_char, password_char);
+  } else {
+    Serial.print("Attempting to connect to open SSID: ");
+    Serial.println(ssid_char);
+    status = WiFi.begin(ssid_char);
+  }
+  int counter = 0;
+  while (status != WL_CONNECTED && counter < 20)
+  {
+    delay(500);
+    status = WiFi.status();
+    counter++;
+  }
+  if (status == WL_CONNECTED) {
+    Serial.println("NodeMCU connected to the network");
+  } else {
+    Serial.println("NodeMCU NOT connected to the network");
+  }
+}
+
+void blink_status() {
+  if (status != WL_CONNECTED) {
+    digitalWrite(pin, LOW);
+    delay(100);
+    digitalWrite(pin, HIGH);
+    delay(100);
+  } else {
+    digitalWrite(pin, HIGH);
+  }
+}
+
+
 
