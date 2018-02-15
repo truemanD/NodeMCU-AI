645348a96ce3f237c3002fefe939007fb68f3c06
truemanD <diyanov.alex@gmail.com>
2018-02-15 09:38:26 +0300

    Add files via upload

--- SmartHomeModules/SmartHomeSensorsTest/SmartHomeSensorsTest.ino
+++ SmartHomeModules/SmartHomeSensorsTest/SmartHomeSensorsTest.ino
@@ -7,12 +7,17 @@
 #include <DHT.h>
 #include <DHT_U.h>
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
+#define DS3231_SCL 1 //---- Переназначаем стандартный пин для Wire.h
+#define DS3231_SDA 3 //---- Переназначаем стандартный пин для Wire.h
 #define DHTPIN  2           //D4
 #define PIN_RX  5           //D1
 #define PIN_TX  4           //D2
 #define DHTTYPE DHT11       //DHT 11 
 
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
 DHT_Unified dht(DHTPIN, DHTTYPE);
 ADC_MODE(ADC_VCC); //vcc read
 SoftwareSerial mySerial(PIN_RX, PIN_TX); // RX,TX
@@ -35,10 +40,15 @@ void setup() {
   initSensors();
   disconnectFromWifi();
   disableAccessPoint();
+
+  Wire.begin(DS3231_SDA, DS3231_SCL); //---- Запускаем I2C на нужных пинах
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
-  postSensorsValues();
+  updateSensorsValues();
+  displayTelemetry();
 }
 
 void initSensors() {
@@ -49,53 +59,77 @@ void initSensors() {
   delayMS = sensor.min_delay / 1000;
 }
 
-void postSensorsValues() {
-    delay(delayInterval);
-    int temp = 0;
-    int ppm = 0;
-    int humid = 0;
-    float vdd = 0;
-    
-    // Temperature
-    sensors_event_t event;
-    dht.temperature().getEvent(&event);
-    if (isnan(event.temperature)) {
-      Serial.println("Error reading temperature!");
-    }
-    else {
-      temp = event.temperature;
-    }
+void updateSensorsValues() {
+  delay(delayInterval);
+  int temp = 0;
+  int ppm = 0;
+  int humid = 0;
+  float vdd = 0;
 
-    // Humidity
-    dht.humidity().getEvent(&event);
-    if (isnan(event.relative_humidity)) {
-      Serial.println("Error reading humidity!");
-    }
-    else {
-      humid = event.relative_humidity;
-    }
+  // Temperature
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println("Error reading temperature!");
+  }
+  else {
+    temp = event.temperature;
+  }
 
-    //  Get voltage
-    {
-      vdd = ESP.getVcc();
-      vdd = vdd / 1000;
-    }
+  // Humidity
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println("Error reading humidity!");
+  }
+  else {
+    humid = event.relative_humidity;
+  }
 
-    //  Get PPM
-    {
-      ppm = getCO2Data();
-    }
-    Serial.print("Temperature: " + String(temp) + "*C; ");
-    Serial.print("Humidity: " + String(humid) + "%; ");
-    Serial.print("CO2: " + String(ppm) + "PPM; ");
-    Serial.print("Voltage: " + String(vdd) + "V;");
-    Serial.println();
+  //  Get voltage
+  {
+    vdd = ESP.getVcc();
+    vdd = vdd / 1000;
+  }
 
-    t_temp = temp;
-    t_humid = humid;
-    t_vdd = vdd;
-    t_ppm = ppm;
-  
+  //  Get PPM
+  {
+    ppm = getCO2Data();
+  }
+
+
+  //lcd.setCursor(1,0);
+  //  lcd.print(humid+" H" + vdd +" V");
+
+  t_temp = temp;
+  t_humid = humid;
+  t_vdd = vdd;
+  t_ppm = ppm;
+
+}
+
+void displayTelemetry() {
+  Serial.print("Temperature: " + String(t_temp) + "*C; ");
+  Serial.print("Humidity: " + String(t_humid) + "%; ");
+  Serial.print("CO2: " + String(t_ppm) + "PPM; ");
+  Serial.print("Voltage: " + String(t_vdd) + "V;");
+  Serial.println();
+
+  lcd.setCursor(0, 0);
+  lcd.print(t_ppm);
+  lcd.setCursor(5, 0);
+  lcd.print("co2");
+  lcd.setCursor(10, 0);
+  lcd.print(t_vdd);
+  lcd.setCursor(14, 0);
+  lcd.print("V");
+  lcd.setCursor(0, 1);
+  lcd.print(t_temp);
+  lcd.setCursor(5, 1);
+  lcd.print("C");
+  lcd.setCursor(10, 1);
+  lcd.print(t_humid);
+  lcd.setCursor(14, 1);
+  lcd.print("%");
 }
 
 int getCO2Data() {
@@ -124,13 +158,13 @@ int getCO2Data() {
     //    int temp = response[4] - 20;
     char raw[32];
     sprintf(raw, "RAW: %02X %02X %02X %02X %02X %02X %02X %02X %02X", response[0], response[1], response[2], response[3], response[4], response[5], response[6], response[7], response[8]);
-//    Serial.println(raw);
-    if (ppm <= 400 || ppm > 4900) {
-      Serial.println("CO2: no valid data");
-      result = -2;
-    } else {
+    //    Serial.println(raw);
+//    if (ppm <= 400 || ppm > 4900) {
+//      Serial.println("CO2: no valid data");
+//      result = -2;
+//    } else {
       result = ppm;
-    }
+//    }
   }
   return result;
 }
@@ -138,7 +172,7 @@ int getCO2Data() {
 
 void disableAccessPoint() {
   Serial.println("Disabling access point!");
-//  Serial.println(tmpOwnSsid);
+  //  Serial.println(tmpOwnSsid);
   WiFi.softAPdisconnect(true);
   Serial.println("ESP AccessPoint disabled");
 }
